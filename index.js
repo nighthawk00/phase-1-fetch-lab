48835acdd972e9d43301046d062f7ed1a04fbb49
Kevin <kevinlopez3292@gmail.com>
2022-12-17 15:52:36 -0500

    commit tree 885e6b838e0087bdb36614acd84f9327337a38c2

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+  window.renderBooks;
   
 }
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  book.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
